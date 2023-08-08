857a02445184940328966161b4796f3dc02f4c92
GeoFrickz <george.maximillian@gmail.com>
2023-08-08 22:52:10 +0700

    register peserta updated

--- registerPesertaStatistik.js
+++ registerPesertaStatistik.js
@@ -8,3 +8,30 @@ window.onscroll = function() {
     }
     prevScrollPos = currentScrollPos;
 }
+
+
+function setupCard(cardIndex) {
+    const uploadButton = document.getElementById(`upload-bukti${cardIndex}`);
+    const fileInput = document.getElementById(`bukti-button${cardIndex}`);
+    const fileNameSpan = document.getElementById(`file-name${cardIndex}`);
+    const fileSelectedMessage = document.getElementById(`file-selected-message${cardIndex}`);
+    
+    uploadButton.addEventListener('click', function(){
+        fileInput.click();
+    });
+
+    fileInput.addEventListener('change', function() {
+        if (fileInput.files.length > 0) {
+            const fileName = fileInput.files[0].name;
+            fileNameSpan.textContent = fileName;
+            fileSelectedMessage.style.display = 'block';
+        } else {
+            fileNameSpan.textContent = '';
+            fileSelectedMessage.style.display = 'none';
+        }
+    });
+}
+
+setupCard(1);
+setupCard(2);
+setupCard(3);
